6f392784535bb5628336710a1a7d5498e6f5c083
dereknoah123 <168110080+dereknoah123@users.noreply.github.com>
2024-04-29 18:26:20 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Java Programming", "Derek Noah", "1234567890");
-        Book book2 = new Book("Python Basics", "Walter White", "0987654321");
-
+  Checkout book1 = new Checkout("Daisy", "48273683","Java Programming", "Derek Noah", "1234567890");
+ Checkout book2 = new Checkout("Dave", "46748627","Python Basics", "Walter White", "0987654321");
         Library library = new Library();
         library.addBook(book1);
         library.addBook(book2);
-
         library.displayBooks();
     }
 }
